e0e0241f03f277cf2514dfcd338689563651795d
BOUCHARD Corentin <corentin.bouchard@etudiant.univ-rennes.fr>
2025-01-27 18:14:06 +0100

    partial factoring of sellables and events

--- progrest/src/factories/FactoryEvent.java
+++ progrest/src/factories/FactoryEvent.java
@@ -12,18 +12,16 @@ import java.util.concurrent.ThreadLocalRandom;
 public class FactoryEvent {
     public Event generateEvent(int act_id){
         Event result;
-        switch(ThreadLocalRandom.current().nextInt(0, 3)){
+        switch(ThreadLocalRandom.current().nextInt(0, 6)){
             case 0:
                 result = new MerchantEncounter(Reader.getStringMerchant(), Reader.getStringLocation());
                 break;
             case 1:
-                result = new Fight(calculateNumberOfMonsters(act_id));
-                break;
-            case 2:
                 result = new Misc();
                 break;
             default:
-                result = null;
+                result = new Fight(calculateNumberOfMonsters(act_id));
+                break;
         };
 
         return result;
--- progrest/src/factories/FactorySellable.java
+++ progrest/src/factories/FactorySellable.java
@@ -1,9 +1,25 @@
 package factories;
 
+import main.Consommable;
+import main.Equipement;
 import main.Sellable;
+import main.Spell;
 
 public class FactorySellable {
     public Sellable generateSellable(){
+
+        return null;
+    }
+
+    public Equipement generateEquipement(){
+        return null;
+    }
+
+    public Spell generateSpell(){
+        return null;
+    }
+
+    public Consommable generateConsommable(){
         return null;
     }
 }
